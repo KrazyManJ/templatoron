7940baaafefd0e960689ab43c09c264018c654c7
KrazyManJ <KrazyManJ@gmail.com>
2023-02-19 16:30:03 +0100

    Moved functionality to OOP

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import templatoron
+from templatoron import TemplatoronObject
 
 if __name__ == '__main__':
-    templatoron.generate_templatoron_file_from_folder(
-        "project_files_to_scan/@#project_name",
-        "templatoron_template_files/pypi-project.json"
-    )
-    templatoron.create_files_from_templatoron_file(
-        "templatoron_template_files/pypi-project.json",
-        "",
-        project_name="projectr",
-        package_name="projectr"
-    )
+    T = TemplatoronObject.from_scaning_folder("project_files_to_scan/@#project_name")
+    T.create_project("",project_name="projectr",package_name="projectr")
--- templatoron.py
+++ templatoron.py
@@ -8,70 +8,96 @@ from cryptography.fernet import Fernet
 
 from tokens import FERNET_KEY
 
-fernet = Fernet(FERNET_KEY)
+FERNET = Fernet(FERNET_KEY)
+EXT = ".json"
+ENC = "utf-8"
 
+class TemplatoronObject:
+    structure: dict = {}
+    variables: list[str] = []
 
-def generate_templatoron_file_from_folder(
-        folder: str | bytes | PathLike,
-        output_projecter_file: str | bytes | PathLike,
-        include_folder: bool = True
-):
-    variables = set({})
+    @staticmethod
+    def from_file(
+            path: str | bytes | PathLike
+    ):
+        def decypt(d: dict):
+            r = {}
+            for k, v in d.items():
+                r[k] = FERNET.decrypt(v.encode(ENC)).decode(ENC) if type(v) == str else v
+            return r
 
-    def var_finder(txt: str):
-        for r in re.findall("(?<=@#)[a-z_A-Z]+", txt):
-            variables.add(r)
-        return txt
+        R = TemplatoronObject()
+        data: dict = json.load(open(path, "r"), object_hook=decypt)
+        R.structure = data.get("structure",{})
+        R.variables = data.get("variables",[])
+        return R
 
-    def folder_scan(parent):
-        r = {}
-        for i, val in enumerate(os.listdir(parent)):
-            pth = var_finder(os.path.join(parent, val))
-            r[val] = folder_scan(pth) \
-                if os.path.isdir(pth) \
-                else fernet.encrypt(var_finder(open(pth, "r").read()).encode("utf-8")).decode("utf-8")
-        return r
+    @staticmethod
+    def from_scaning_folder(
+            folder: str | bytes | PathLike,
+            include_folder: bool = True
+    ):
+        variables = set({})
 
-    STRUCTURE = {os.path.basename(folder): folder_scan(folder)} if include_folder else folder_scan(folder)
-    json.dump({"variables": list(variables), "structure": STRUCTURE}, open(output_projecter_file, "w"), indent=4)
+        def var_finder(txt: str):
+            for r in re.findall("(?<=@#)[a-z_A-Z]+", txt):
+                variables.add(r)
+            return txt
 
+        def folder_scan(parent):
+            r = {}
+            for i, val in enumerate(os.listdir(parent)):
+                pth = var_finder(os.path.join(parent, val))
+                r[val] = folder_scan(pth) if os.path.isdir(pth) else var_finder(open(pth, "r").read())
+            return r
 
-def create_files_from_templatoron_file(
-        file: str | bytes | PathLike,
-        output_path: str | bytes | PathLike,
-        **variable_values
-):
-    def parse_json_func(d: dict):
-        r = {}
-        for k, v in d.items():
-            r[k] = fernet.decrypt(v.encode("utf-8")).decode("utf-8") if type(v) == str else v
-        return r
+        R = TemplatoronObject()
+        R.structure = {os.path.basename(folder): folder_scan(folder)} if include_folder else folder_scan(folder)
+        R.variables = list(variables)
+        return R
 
-    def var_parser(txt: str):
-        for k, v in variable_values.items():
-            txt = txt.replace(f"@#{k}", v)
-        return txt
+    def save(
+            self,
+            path: str | bytes | PathLike
+    ):
+        def encrypt(data):
+            for k, v in data.items():
+                if isinstance(v, str):
+                    data[k] = FERNET.encrypt(v.encode(ENC)).decode(ENC)
+                elif isinstance(v, dict):
+                    encrypt(v)
+            return data
 
-    def file_creator(parent, file_or_folder_dict: dict):
-        for k, v in file_or_folder_dict.items():
-            fname = os.path.join(parent, var_parser(k))
-            if type(v) is dict:
-                os.mkdir(fname)
-                file_creator(fname, v)
-            elif type(v) is str:
-                open(fname, "w").write(var_parser(v))
+        RESULT = {
+            "variables": self.variables,
+            "structure": self.structure
+        }
+        json.dump(encrypt(RESULT),open(path,"w"),indent=4)
 
-    if not os.path.exists(file):
-        raise FileNotFoundError(f"File \"{file}\" does not exist!")
-    if not file.endswith(".json"):
-        raise Exception("File is not templatoron file (.json)!")
-    data: dict = json.load(open(file, "r"), object_hook=parse_json_func)
-    verify_templatoron_json(data)
-    srcvarset = set(data.get("variables", []))
-    varset = set(variable_values.keys())
-    if varset != srcvarset:
-        raise Exception("Missing variables: " + ", ".join(srcvarset.difference(varset)))
-    file_creator(output_path, data.get("structure", {}))
+    def create_project(
+            self,
+            output_path: str | bytes | PathLike,
+            **variable_values: str
+    ):
+        def var_parser(txt: str):
+            for k, v in variable_values.items():
+                txt = txt.replace(f"@#{k}", v)
+            return txt
+
+        def file_creator(parent, file_or_folder_dict: dict):
+            for k, v in file_or_folder_dict.items():
+                fname = os.path.join(parent, var_parser(k))
+                if type(v) is dict:
+                    os.mkdir(fname)
+                    file_creator(fname, v)
+                elif type(v) is str:
+                    open(fname, "w").write(var_parser(v))
+
+        srcvarset = set(self.variables)
+        varset = set(variable_values.keys())
+        if varset != srcvarset:
+            raise Exception("Missing variables: " + ", ".join(srcvarset.difference(varset)))
+        file_creator(output_path, self.structure)
 
 
 def verify_templatoron_json(data: dict):
